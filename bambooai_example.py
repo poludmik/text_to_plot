3b1854f057b6ef4583dbcb4adc5bcc8b3d9906ab
poludmik <michael.poludin@gmail.com>
2023-10-26 15:27:39 +0200

    Tests

--- bambooai_example.py
+++ bambooai_example.py
@@ -10,4 +10,6 @@ df = pd.read_csv('data/country_happiness_gdp.csv')
 # bamboo = BambooAI(df, debug=False, vector_db=False, exploratory=False, llm_switch_plan=False, llm_switch_code=True, search_tool=False)
 bamboo = BambooAI(df, debug=False, vector_db=False, exploratory=True, llm_switch_plan=False, search_tool=False, local_code_model='CodeLlama-7B-Python-fp16')
 
+
+
 bamboo.pd_agent_converse("Find top 7 least happy countries.")
